b658e1e5d1168b19ac97ee800905b67c12f3b5fd
damianhinch <damianhinch@gmail.com>
2015-12-08 12:41:27 +0100

    Solution to Problem 10

--- python/src/problem010.py
+++ python/src/problem010.py
@@ -4,10 +4,14 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 '''
 
-from primeUtilities import is_prime
+from primeUtilities import primes_in_range
 
-max_range = 1000000
+max_range = 2000000
 sum = 0
 
-for i in range(1, max_range, 2):
-    if is_prime()
+number_list = range(1, max_range)
+number_list_prime = primes_in_range(number_list)
+sum = 0
+for prime in number_list_prime:
+    sum += prime
+print(sum)
